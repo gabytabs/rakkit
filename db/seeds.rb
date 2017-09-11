77c6cd0f9910d0f5e753354d9e43244007ec8480
Mar Gabriel Tabilla <gabytabilla@gmail.com>
2017-09-11 18:53:29 -0400

    seeded user for testing with relationships

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,5 @@
 (1..10).each do
   Blog.create(title: Faker::Beer.name, content: Faker::Hacker.say_something_smart)
 end
+
+User.create(email: "test@test.com", password: "123456", password_confirmation: "123456")
